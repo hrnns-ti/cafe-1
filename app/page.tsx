988d89447f0f434ca931a1f99b350fcc13c72679
Haerunnas <nassjourney@gmail.com>
2026-04-11 11:58:41 +0700

    Feat: Section 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import { inter, notoSerifJP } from "@/fonts/fonts";
+import { inter, notoSerifJP, notoSans } from "@/fonts/fonts";
 import { useState, useEffect } from "react";
 
 import Navbar from "@/components/navbar";
@@ -12,10 +12,24 @@ const backgroundImages = [
   '/section-1/3.jpg'
 ]
 
+const carouselCol1 = [
+  '/section-2/carousel-5.jpg',
+  '/section-2/carousel-1.jpg',
+  '/section-2/carousel-2.jpg',
+]
+
+const carouselCol2 = [
+  '/section-2/carousel-4.jpg',
+  '/section-2/carousel-3.jpg',
+  '/section-2/carousel-6.jpg',
+]
+
 export default function Home() {
   
   const [currentBG, setCurrentBG] = useState(0)
   const nextBG = (currentBG + 1) % backgroundImages.length
+
+  const [carousel, setCarousel] = useState()
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -26,44 +40,95 @@ export default function Home() {
   },[])
   
   return (
-    <div className="flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 font-sans ">
+      <main className="flex flex-1 w-full flex-col sm:items-start">
         <Navbar />
-        <div className="flex flex-col w-full py-36 px-16">
-          {backgroundImages.map((src, index) => (
-            <Image
-              key={src}
-              src={src}
-              alt={`Background ${index + 1}`} 
-              fill={true}
-              objectFit="cover"
-              priority={index === 0}
-              className={`
-                saturate-50 transition-opacity duration-1000 ease-in-out 
-                ${index === nextBG ? "opacity-100 z-0" : "opacity-0 z-0"}
-                ${index === currentBG ? "opacity-100 z-10" : "opacity-0 z-10"}
-              `}
-            />
-          ))}
-          <div className="flex flex-col py-6 select-none z-20 text-white w-full">
+        {/* section 1 */}
+        <section className="relative w-full min-h-screen overflow-hidden">
+          <div className="absolute inset-0 -z-10">
+            {backgroundImages.map((src, index) => (
+              <Image
+                key={src}
+                src={src}
+                alt={`Background ${index + 1}`} 
+                fill={true}
+                objectFit="cover"
+                priority={index === 0}
+                className={`
+                  absolute inset-0 saturate-50  transition-opacity duration-1000 ease-in-out 
+                  ${index === nextBG ? "opacity-100" : "opacity-0"}
+                  ${index === currentBG ? "opacity-100" : "opacity-0"}
+                `}
+              />
+            ))}
+          </div>
+
+          <div className="relative flex flex-col px-12 py-36 select-none z-10 text-white w-full">
             <p className={`${inter.className} py-3 text-xs tracking-[6]`}>VOL. XXIV —— TOKYO EDITION</p>
             <h1 className={`${notoSerifJP.className} text-9xl font-black`}>デイリー</h1>
             <h1 className={`${notoSerifJP.className} text-9xl font-black`}>ブリュー。</h1>
             <p className={`${notoSerifJP.className} py-12 text-xl`}>日本の新聞紙の触覚的な厳格さと、朝の静か<br/>な注ぎの儀式に敬意を表して。</p>
           </div>
-        </div>
 
+          <div className="absolute bottom-0 w-full overflow-x-hidden">
+            <div className={`flex flex-row gap-2 text-xs m-4 font-semibold ${notoSerifJP.className}`}>
+              <p className="px-4 py-2 rounded-full bg-white">以来 12.02.1995</p>
+              <p className="px-4 py-2 rounded-full bg-white">Kizu Cafe (キズカフェ) は2026年にこれまでで20万人の来店客に達しました。</p>
+            </div>
+            <div className="flex whitespace-nowrap w-max animate-marquee bg-gray-900">
+              <p className={`text-white ${notoSerifJP.className}`}>本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • 本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • シングルオリジン 京都抹茶 • 在庫状況</p>
+              <p className={`text-white ${notoSerifJP.className}`}>本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • 本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • シングルオリジン 京都抹茶 • 在庫状況</p>
+            </div>
+          </div>
+        </section>
 
-        <div className="absolute bottom-0 w-full overflow-hidden z-20">
-          <div className={`flex flex-row gap-2 text-xs m-4 font-semibold ${notoSerifJP.className}`}>
-            <p className="px-4 py-2 rounded-full bg-white z-20">以来 12.02.1995</p>
-            <p className="px-4 py-2 rounded-full bg-white z-20">Kizu Cafe (キズカフェ) は2026年にこれまでで20万人の来店客に達しました。</p>
+        {/* section 2 */}
+        <section className="w-full min-h-screen flex flex-row">
+          <div className="min-h-screen w-3/5 pt-16 pb-8 px-6 justify-between flex flex-col">
+            <div className="flex flex-col">
+              <h1 className={`${notoSerifJP.className} font-bold text-9xl`}>製品</h1>
+              <p className={`${notoSerifJP.className} font-bold py-8`}>新鮮で毎日手作りのアイテムと、あなたのフィードを確実に映え<br/>させる美的センスの完璧な組み合わせ。</p>
+            </div>
+            <div className="flex flex-col w-auto">
+              <p className={`flex ${notoSerifJP.className} items-center font-black`}>おはよう 01  
+                <span className="border-b-2 border-black h-0 mx-1 w-18"/> 03
+              </p>
+              <h1 className={`font-bold text-4xl tracking-wider ${notoSans.className} pb-8`}>DAILY LEDGER</h1>
+              <p className={`${notoSerifJP.className} font-medium`}>毎朝、フレッシュで新しい日次台帳をお届けすることにこだわっています<br />—目に楽しいモダンな美学で丁寧に作られており、フォロワー全員と共有<br />したくなるほど忘れられない体験を提供します。</p>
+            </div>
           </div>
-          <div className="flex whitespace-nowrap w-max animate-marquee bg-gray-900">
-            <p className={`text-white ${notoSerifJP.className}`}>本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • 本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • シングルオリジン 京都抹茶 • 在庫状況</p>
-            <p className={`text-white ${notoSerifJP.className}`}>本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • 本日のスペシャル：シングルオリジン 京都抹茶 • 在庫状況：エチオピア産豆（少）• 新入荷：季節のいちごスフレ • シングルオリジン 京都抹茶 • 在庫状況</p>
+          
+          <div className="relative w-2/5 h-screen flex flex-row gap-4 overflow-hidden">
+            <div className="w-1/2 flex flex-col gap-4 h-max pb-4 animate-carousel-up">
+              {[...carouselCol1, ...carouselCol1].map((src, index) => (
+                <div key={`col2-${index}`} className="relative w-full aspect-3/4 shrink-0">
+                  <Image 
+                    src={src} 
+                    alt={`Product Col 1 - ${index}`} 
+                    fill={true}
+                    className="object-cover rounded-sm relative" 
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="w-1/2 flex flex-col gap-4 animate-carousel-down h-max -mt-50">
+              {[...carouselCol2, ...carouselCol2].map((src, index) => (
+                <div key={`col1-${index}`} className="relative w-full aspect-3/4 shrink-0">
+                  <Image 
+                    src={src} 
+                    alt={`Product Col 1 - ${index}`} 
+                    fill ={true}
+                    className="object-cover rounded-sm absolute" 
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        </section>
+
+        <section className="w-full min-h-screen bg-black">
+
+        </section>
       </main>
     </div>
   );
--- fonts/fonts.tsx
+++ fonts/fonts.tsx
@@ -1,4 +1,4 @@
-import { Noto_Serif_JP, Inter } from "next/font/google";
+import { Noto_Serif_JP, Inter, Noto_Sans } from "next/font/google";
 
 export const notoSerifJP = Noto_Serif_JP({
   weight: ['400', '700'],
@@ -9,4 +9,9 @@ export const notoSerifJP = Noto_Serif_JP({
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
+})
+
+export const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  display: 'swap'
 })
